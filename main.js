be91b8dc9b3eef3c66ede48962b8c0a6a79c14d3
Oussama Adame <oussama.adame12@gmail.com>
2023-11-29 23:25:35 +0100

    Update main.js
    fix : top level

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ import "./style.css"; // Vite handles inserting CSS
 // Common pattern is to put JS source files in src/
 import { setupVideo, takePhoto } from "./src/camera.js";
 
+
+(async function run(){
+  
 const button = document.querySelector("#take-photo");
 const canvas = document.querySelector("canvas");
 const video = await setupVideo();
 
 button.addEventListener("click", () => {
   takePhoto(video, canvas);
-});
+})
+})();
